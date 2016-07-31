642a57657fda038f6dbac81b00497df700aac790
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-08-01 00:19:37 +0530

    Added 16 bit operations

---
+++ Lab1/add_16.asm
@@ -0,0 +1,69 @@
+ORG 0000H
+LJMP MAIN
+
+;R0 and R1 should contain the address of two no.s
+;location given by R0:- 	MSB of 1st no.
+;location given by R0+1:-	LSB of 1st no.
+;location given by R1:- 	MSB of 1st no.
+;location given by R1+1:-	LSB of 1st no.
+;location given by R0+2:- 	CARRY	
+;location given by R0+3:-	MSB OF ANS	
+;location given by R0+4:- 	LSB OF ANS
+
+;---------------------------------------------------------;
+;this function adds and stores result in appropriate location
+ADDER_16BIT:
+	;-- push the registers which will be affected by this subroutine 
+	;	but will be needed later 	
+	
+	;-- perform the addition/subtraction of 2 16-bit no.s
+	;-- you may use subroutine wrtten for addition of 2 8-bit no.s
+	;-- remember the no.s are given in 2's complement form
+	
+	;-- take care when you set carry/borrow.
+	
+	;-- store the result at appropriate locations.
+	MOV A, 60h
+	RLC A
+	JNC next1
+	MOV R2,#01h
+	next1:
+	RRC A
+	MOV R3,A
+	MOV A, 70h
+	RLC A
+	JNC next2
+	MOV R4,#01h
+	next2: 
+	RRC A
+	MOV R5,A
+	
+	MOV A, 61h
+	ADD A, 71h
+	MOV 64h, A
+	MOV A, R3
+	ADDC A, R5
+	MOV 63h, A
+	
+	MOV A,R2
+	ADDC A,R4
+	ANL A,#01h
+	MOV 62h, A
+	RETURN:
+	RET
+
+INIT:
+	;-- store the numbers to be added/subtracted at appropriate location
+	MOV 60h,#0FFh
+	MOV 61h,#00h
+	MOV 70h,#001h
+	MOV 71h,#00h
+	RET
+
+
+ORG 0100H
+MAIN:
+	MOV SP,#0C0H	;move stack pointer to indirect RAM location
+	ACALL INIT
+	ACALL ADDER_16BIT
+END
---
+++ Lab1/subtract_16.asm
@@ -0,0 +1,80 @@
+ORG 0000H
+LJMP MAIN
+
+;R0 and R1 should contain the address of two no.s
+;location given by R0:- 	MSB of 1st no.
+;location given by R0+1:-	LSB of 1st no.
+;location given by R1:- 	MSB of 1st no.
+;location given by R1+1:-	LSB of 1st no.
+;location given by R0+2:- 	CARRY	
+;location given by R0+3:-	MSB OF ANS	
+;location given by R0+4:- 	LSB OF ANS
+
+;---------------------------------------------------------;
+;this function adds and stores result in appropriate location
+ADDER_16BIT:
+	;-- push the registers which will be affected by this subroutine 
+	;	but will be needed later 	
+	
+	;-- perform the addition/subtraction of 2 16-bit no.s
+	;-- you may use subroutine wrtten for addition of 2 8-bit no.s
+	;-- remember the no.s are given in 2's complement form
+	
+	;-- take care when you set carry/borrow.
+	
+	;-- store the result at appropriate locations.
+	MOV A, 60h
+	RLC A
+	JNC next1
+	MOV R2,#01h
+	next1:
+	RRC A
+	MOV R3,A
+	MOV A, 70h
+	RLC A
+	JNC next2
+	MOV R4,#01h
+	next2: 
+	RRC A
+	MOV R5,A
+	
+	MOV A, 61h
+	ADD A, 71h
+	MOV 64h, A
+	MOV A, R3
+	ADDC A, R5
+	MOV 63h, A
+	
+	MOV A,R2
+	ADDC A,R4
+	ANL A,#01h
+	MOV 62h, A
+	RETURN:
+	RET
+
+INIT:
+	;-- store the numbers to be added/subtracted at appropriate location
+	MOV 60h,#0FFh
+	MOV 61h,#00h
+	MOV 70h,#0FFh
+	MOV 71h,#01h
+	RET
+
+TWO_COMPLEMENT:
+	MOV A,71h
+	CPL A
+	ADD A,#01h
+	MOV 71h, A
+	MOV A,70h
+	CPL A
+	ADDC A,#00h
+	MOV 70h, A
+	RET
+
+ORG 0100H
+MAIN:
+	MOV SP,#0C0H	;move stack pointer to indirect RAM location
+	ACALL INIT
+	ACALL TWO_COMPLEMENT
+	ACALL ADDER_16BIT
+END
