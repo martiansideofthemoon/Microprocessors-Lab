a5dd1c126793b46b6f59aa78c217f2850f56d528
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-08-27 20:05:14 +0530

    Implementing delay with an ISR

---
+++ Lab5/delay_interrupt.asm
@@ -0,0 +1,38 @@
+org 00h
+	ljmp main
+org 000Bh
+	sjmp isr_timer0
+org 50h
+isr_timer0:
+	INC R1
+	CLR TR0
+	CLR TF0
+	MOV TL0, #00h
+	MOV TH0, #00h
+	SETB TR0
+	reti
+delay_1s:
+	SETB EA
+	SETB ET0
+	USING 0
+	PUSH AR1
+	MOV TMOD, #01h
+	MOV TL0, #00h
+	MOV TH0, #00h
+	SETB TR0
+this:
+	CJNE R1, #1Fh, this
+	CLR ET0
+	CLR EA
+	POP AR1
+	ret
+
+
+org 100h
+main:
+	acall delay_1s
+stop:
+	sjmp stop
+
+	
+END
