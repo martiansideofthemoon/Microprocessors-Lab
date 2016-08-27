2d0022137ef47908136fac97449f856550a0c66b
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-08-27 19:46:17 +0530

    Adding labwork 5

---
+++ Lab5/delay_polling.asm
@@ -0,0 +1,31 @@
+org 00h
+	ljmp main
+org 50h
+delay_1s:
+	USING 0
+	PUSH AR1
+	MOV R1, #1Fh
+timer:
+	MOV TMOD, #01h
+	MOV TL0, #00h
+	MOV TH0, #00h
+	SETB TR0
+	acall poll
+	CLR TR0
+	CLR TF0
+	DJNZ R1, timer
+	POP AR1
+	ret
+	
+poll:
+	JNB TF0, poll
+	ret
+
+org 100h
+main:
+	acall delay_1s
+stop:
+	sjmp stop
+
+	
+END
